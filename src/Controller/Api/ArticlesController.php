b9a11517f0f584712f36541c527933ce7c7b03f8
gpabloandres <gpabloandres@gmail.com>
2016-07-11 11:39:00 -0300

    Acceso sin Token para index y view de Articles y Events

--- src/Controller/Api/ArticlesController.php
+++ src/Controller/Api/ArticlesController.php
@@ -5,6 +5,12 @@ use App\Controller\Api\AppController;
 
 class ArticlesController extends AppController
 {
+    public function initialize()
+    {
+        parent::initialize();
+        $this->Auth->allow(['index', 'view']);
+    }
+
     public $paginate = [
         'page' => 1,
         'limit' => 5,
